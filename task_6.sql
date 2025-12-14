5f9bec3af77e93d87c56fc5ec1d12ace5ec5a504
abyalewtetsafe@gmail.com <abyalewtetsafe@gmail.com>
2025-12-14 20:54:50 +0300

    Populating Your Database with Your More Data

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 -- Explicitly select the database (for robustness)
 USE ALX_BOOK_STORE;
 
--- Insert multiple rows into the 'customer' table
+-- Insert multiple rows into the 'customer' table, including the required double spaces in the address
 INSERT INTO customer (
     customer_id,
     customer_name,
@@ -13,17 +13,17 @@ VALUES
     2,
     'Blessing Malik',
     'bmalik@sandtech.com',
-    '124 Happiness Ave.'
+    '124 Happiness  Ave.'  -- <-- Double space here
 ),
 (
     3,
     'Obed Ehoneah',
     'eobed@sandtech.com',
-    '125 Happiness Ave.'
+    '125 Happiness  Ave.'  -- <-- Double space here
 ),
 (
     4,
     'Nehemial Kamolu',
     'nkamolu@sandtech.com',
-    '126 Happiness Ave.'
+    '126 Happiness  Ave.'  -- <-- Double space here
 );
