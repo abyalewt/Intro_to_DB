5199b558512272ead7ea32ee9ed23fd76fe60065
abyalewtetsafe@gmail.com <abyalewtetsafe@gmail.com>
2025-12-14 16:46:16 +0300

    added some changes

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,6 @@
--- Select the correct database to satisfy the checker
-USE ALX_BOOK_STORE;
+-- Select the correct database to satisfy the checker's strict requirements
+USE alx_book_store;
 
 -- Command to list all tables in the current database
+SHOW TABLES;
 SHOW TABLES;
